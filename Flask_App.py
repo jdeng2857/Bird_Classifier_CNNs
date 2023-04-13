1dd48d73f2cee72435d022fb028d89526a7ec1aa
Jerry Deng <jdeng2857@gmail.com>
2023-04-13 07:10:02 -0400

    Top 5 predictions work

--- Flask_App.py
+++ Flask_App.py
@@ -9,10 +9,11 @@ import pandas as pd
 import numpy as np
 from PIL import Image
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 app.config['SECRET_KEY'] = 'supersecretkey'
 app.config['UPLOAD_FOLDER'] = 'static/files'
 app.config['CLASS_FOLDER'] = 'classes'
+
 
 def all_class_images():
     csv_df = pd.read_csv("birds.csv")
@@ -23,20 +24,26 @@ def all_class_images():
     train_filepaths = filepaths[:81950]
     id_to_filepaths = dict(zip(train_ids, train_filepaths))
     return id_to_filepaths
+
+
 CLASS_IMAGES = all_class_images()
+
 
 class UploadFileForm(FlaskForm):
     file = FileField("File", validators=[InputRequired()])
     submit = SubmitField("Upload and Classify Image")
 
+
 @app.route('/uploads/<filename>')
 def get_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
 
 @app.route('/classes/<url>')
 def get_class_image(url):
     return send_from_directory(app.config['CLASS_FOLDER'], url)
     # return send_from_directory(app.config['CLASS_FOLDER'], filename)
+
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/home', methods=['GET', 'POST'])
@@ -50,8 +57,12 @@ def home():
             secure_filename(file.filename)
         )
         file.save(filepath)
-        predicted_id, predicted_class = classifyImage(filepath)
+        predicted_id, predicted_class, predicted_prob, top_ids, top_probs, top_classes = classifyImage(filepath)
         image_url = "/uploads/" + file.filename
+
+        class_images = []
+        for id in top_ids:
+            class_images.append("/classes/" + str(int(id)) + ".jpg")
 
         class_image_url = "/classes/" + str(int(predicted_id)) + ".jpg"
         print("class image url")
@@ -61,11 +72,17 @@ def home():
             'index.html',
             form=form,
             image_url=image_url,
-            class_image_url = class_image_url,
-            predicted_class=predicted_class
+            class_image_url=class_image_url,
+            predicted_class=predicted_class,
+            predicted_prob=predicted_prob,
+            top_ids=top_ids,
+            top_probs=top_probs,
+            top_classes=top_classes,
+            class_images=class_images
         )
 
     return render_template('index.html', form=form)
+
 
 def classifyImage(filepath):
     one_epoch_model = tf.keras.models.load_model("batch_normalized_5_epochs")
@@ -76,6 +93,13 @@ def classifyImage(filepath):
     predict_x = one_epoch_model.predict(image_array)
     classes_x = np.argmax(predict_x, axis=1)
     print(classes_x)
+    predictions = predict_x[0]
+    top_indices = [i for i in np.argsort(predictions)[-5:]]
+    top_indices.reverse()
+    print("top indices")
+    print(top_indices)
+    top_probs = [predictions[i] for i in top_indices]
+    print(top_probs)
 
     # Generate class labels
     csv_df = pd.read_csv("birds.csv")
@@ -83,10 +107,14 @@ def classifyImage(filepath):
     labels = dataset[:, 2]
     class_ids = dataset[:, 0]
     id_to_names = dict(zip(class_ids, labels))
+
     predicted_id = classes_x[0]
     predicted_class = id_to_names[classes_x[0]]
+    predicted_prob = top_probs[0]
+    top_classes = [id_to_names[i] for i in top_indices]
+    print(top_classes)
 
-    return predicted_id, predicted_class
+    return predicted_id, predicted_class, predicted_prob, top_indices, top_probs, top_classes
 
 if __name__ == '__main__':
     app.run(debug=True)
